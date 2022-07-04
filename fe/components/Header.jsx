30ad18a7f41ec00f5b41911425fd20b5d84c5ab4
Kuro0No <90337201+Kuro0No@users.noreply.github.com>
2022-07-04 23:14:35 +0700

    1

--- fe/components/Header.jsx
+++ fe/components/Header.jsx
@@ -1,29 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import style from '../styles/Header.module.scss'
 import Link from 'next/link';
-const { Search } = Input;
 import { useSelector } from 'react-redux'
 import { useRouter } from 'next/router'
 import { forwardRef } from 'react';
-import { Typography, Input, Badge } from 'antd';
+import { Typography, Input, Badge, List, Avatamr, Cascader } from 'antd';
 import { UserOutlined, HeartOutlined, ShoppingCartOutlined } from '@ant-design/icons'
+const { Search } = Input;
 
 
 
 const Header = () => {
   const { cart } = useSelector(state => state.cart)
   const router = useRouter()
-  const {query} = router
+  const { query } = router
+  const [search, setSearch] = useState()
   const onSearch = (e) => {
-    router.push({
-      query: {
-        ...(query.category) && {category: query.category },
-        ...(query.sort) && {sort: query.sort },
-        ...(query.color) && {color: query.color },
-        search: e
-      }
-    })
+    setSearch(e)
+    // router.push({
+    //   query: {
+    //     ...(query.category) && { category: query.category },
+    //     ...(query.sort) && { sort: query.sort },
+    //     ...(query.color) && { color: query.color },
+    //     search: e
+    //   }
+    // })
   }
+  const data = [
+    {
+      title: 'Ant Design Title 1',
+    },
+    {
+      title: 'Ant Design Title 2',
+    },
+    {
+      title: 'Ant Design Title 3',
+    },
+    {
+      title: 'Ant Design Title 4',
+    },
+  ];
+
   return (
     <div className={style.container}>
 
@@ -38,9 +55,23 @@ const Header = () => {
           <a >Shop</a>
         </Link>
       </Typography.Title>
-
-
-      <Search onSearch={(e) => onSearch(e)} className={style.input} size='large' placeholder="input search text" />
+      <div className={style.search}>
+        <Search onSearch={(e) => onSearch(e)} className={style.input} size='large' placeholder="input search text" />
+        <List
+          className={style.showSearch}
+          itemLayout="horizontal"
+          dataSource={data}
+          renderItem={item => (
+            <List.Item>
+              <List.Item.Meta
+                // avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+                title={<a href="https://ant.design">{item.title}</a>}
+                description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+              />
+            </List.Item>
+          )}
+        />
+      </div>
       <div className={style.right}>
         <Link href="/login">
           <UserOutlined className={style.icon} />
